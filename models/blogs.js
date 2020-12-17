c08f1d24f15ab5be592a592b62fcde231f77c9eb
swaatipandey <swati.pandey@contentstack.com>
2020-12-17 10:56:33 +0530

    added blog class in models folder

---
+++ models/blogs.js
@@ -0,0 +1,9 @@
+class Blogs {
+  constructor(id, author, title, content) {
+    this.id = id;
+    this.author = author;
+    this.title = title;
+    this.content = content;
+  }
+}
+module.exports = Blogs;
